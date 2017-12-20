d9e5c05b9a9e9eccc5388eceb249fb83d3a132c1
Desrumaux <guillaume.desrumaux@isen.yncrea.fr>
2017-12-20 10:52:45 +0100

    Reformat code

--- src/edu/isen/fhgd/fft/FFT.java
+++ src/edu/isen/fhgd/fft/FFT.java
@@ -5,71 +5,95 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * \file Fft.java
+ * \file FFT.java
  * Class contenant les diffèrentes fft
  */
-public class Fft {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Fft.class);
-
-    private int tailleP2;
-    private Complexe[] signal;
-    private Complexe[] sortie;
-
-    private float[] signalR;
-    private float[] sortieR;
+public class FFT {
+    /**
+     * Logger
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(FFT.class);
 
     /**
+     * Taille du tableau (correspond à une puissance de 2)
+     */
+    private int tailleP2;
+
+    /**
+     * Signal complexe d'entrée
+     */
+    private Complexe[] signal;
+
+    /**
+     * Sortie de l'algorithme
+     */
+    private Complexe[] sortie;
+
+    /**
+     * Signal d'entrée réel
+     */
+    private float[] signalR;
+
+    /**
+     * Constructeur d'objet à partir d'un signal complexe
+     *
      * @param tailleP2
      * @param signal
      */
-    public Fft(int tailleP2, Complexe[] signal) throws IllegalArgumentException {
-        if (testPuissance2(tailleP2)) {
+    public FFT(int tailleP2, Complexe[] signal) throws IllegalArgumentException {
+        if (estPuissance2(tailleP2)) {
             this.tailleP2 = tailleP2;
             this.signal = signal;
-            LOGGER.info("Création d'un objet FFT");
+            LOGGER.debug("Création d'un objet FFT de taille : " + tailleP2);
         } else {
-            LOGGER.error("La taille doit être une puissance de 2 !");
-            throw new IllegalArgumentException("La taille n'est pas une puissance de 2");
+            LOGGER.error("La taille " + tailleP2 + " doit être une puissance de 2 !");
+            throw new IllegalArgumentException("La taille " + tailleP2 + " doit être une puissance de 2 !");
         }
     }
 
     /**
+     * Constructeur d'objet à partir d'un signal réel
+     *
      * @param tailleP2
      * @param tabReel
      */
-    public Fft(int tailleP2, float[] tabReel) throws IllegalArgumentException {
-        if (testPuissance2(tailleP2)) {
+    public FFT(int tailleP2, float[] tabReel) throws IllegalArgumentException {
+        if (estPuissance2(tailleP2)) {
             this.tailleP2 = tailleP2;
             this.signalR = tabReel;
-            LOGGER.info("Création d'un objet FFT");
+            LOGGER.debug("Création d'un objet FFT de taille : " + tailleP2);
         } else {
-            LOGGER.error("La taille doit être une puissance de 2 !");
-            throw new IllegalArgumentException("La taille n'est pas une puissance de 2");
+            LOGGER.error("La taille " + tailleP2 + " doit être une puissance de 2 !");
+            throw new IllegalArgumentException("La taille " + tailleP2 + " doit être une puissance de 2 !");
         }
     }
 
     /**
-     * Cette méthode calcule une fft réel
+     * Méthode calculant une FFT à partir de valeurs réelles
+     *
+     * @return
      */
-    public Complexe[] fftRapideReel() {
-        this.sortieR = new float[this.tailleP2];
+    public Complexe[] FFTRelle() {
+        this.sortie = new Complexe[this.tailleP2];
         if (tailleP2 == 1) {
-            this.sortieR[0] = this.signalR[0];
+            this.sortie[0] = new Complexe(this.signalR[0], 0);
         } else {
             float[] tabPairs = new float[tailleP2 / 2];
             float[] tabImpairs = new float[tailleP2 / 2];
 
-            for (int i = 0; i < this.tailleP2 / 2; i++){
+            for (int i = 0; i < this.tailleP2 / 2; i++) {
                 tabPairs[i] = this.signalR[2 * i];
                 tabImpairs[i] = this.signalR[2 * i + 1];
             }
 
-            Fft paire = new Fft(this.tailleP2 / 2, tabPairs);
-            Fft impaire = new Fft(this.tailleP2 / 2, tabImpairs);
-            //On calcule la FFT des deux listes
-            paire.fftRapideReel();
-            impaire.fftRapideReel();
+            FFT paire = new FFT(this.tailleP2 / 2, tabPairs);
+            FFT impaire = new FFT(this.tailleP2 / 2, tabImpairs);
+            LOGGER.debug("Calcul transofrmées de Fourrier pour tableau paires et tableau impaires");
+            // On calcule la FFT des deux listes
+            paire.FFTRelle();
+            impaire.FFTRelle();
 
+            LOGGER.debug("Remplissage tableau final");
             for (int k = 0; k <= (tailleP2 / 2) - 1; k++) {
                 Complexe M = new Complexe((float) (-2 * Math.PI * k) / tailleP2);
                 this.sortie[k] = (paire.sortie[k]).addition(impaire.sortie[k].multiplication(M));
@@ -80,9 +104,11 @@ public class Fft {
     }
 
     /**
-     * Cette méthode calcule une fft complèxe
+     * Méthode calculant une FFT à partir de valeurs complexes
+     *
+     * @return
      */
-    public Complexe[] fftRapideComplexe() {
+    public Complexe[] FFTComplexe() {
         this.sortie = new Complexe[this.tailleP2];
         if (tailleP2 == 1) {
             this.sortie[0] = this.signal[0];
@@ -95,12 +121,14 @@ public class Fft {
                 tabImpairs[i] = this.signal[2 * i + 1];
             }
 
-            Fft paire = new Fft(this.tailleP2 / 2, tabPairs);
-            Fft impaire = new Fft(this.tailleP2 / 2, tabImpairs);
-            //On calcule la FFT des deux listes
-            paire.fftRapideComplexe();
-            impaire.fftRapideComplexe();
+            FFT paire = new FFT(this.tailleP2 / 2, tabPairs);
+            FFT impaire = new FFT(this.tailleP2 / 2, tabImpairs);
+            LOGGER.debug("Calcul transofrmées de Fourrier pour tableau paires et tableau impaires");
+            // On calcule la FFT des deux listes
+            paire.FFTRelle();
+            impaire.FFTRelle();
 
+            LOGGER.debug("Remplissage tableau final");
             for (int k = 0; k <= (tailleP2 / 2) - 1; k++) {
                 Complexe M = new Complexe((float) (-2 * Math.PI * k) / tailleP2);
                 this.sortie[k] = (paire.sortie[k]).addition(impaire.sortie[k].multiplication(M));
@@ -111,13 +139,18 @@ public class Fft {
     }
 
     /**
+     * Méthode calculant la FFT inverse
      *
+     * @return
      */
     public Complexe[] inverseFFT() {
+        LOGGER.debug("Remplissage du tableau de signal avec les conjugués");
         for (int i = 0; i < signal.length; i++) {
             signal[i] = signal[i].conjugue();
         }
-        this.fftRapideComplexe();
+        LOGGER.debug("Calcul de la FFT");
+        this.FFTComplexe();
+        LOGGER.debug("Remplissage du tableau de sortie avec le conjugué");
         for (int i = 0; i < this.sortie.length; i++) {
             this.sortie[i] = this.sortie[i].conjugue();
             this.sortie[i] = this.sortie[i].multiplication(new Complexe(1 / (float) this.sortie.length, 0));
@@ -126,18 +159,20 @@ public class Fft {
     }
 
     /**
-     * Test si la valeur est bien une puissance de 2
+     * Méthode vérifiant si une valeur correspond bien à une puissance de 2
      *
      * @param value
      * @return
      */
-    private boolean testPuissance2(int value) {
+    private boolean estPuissance2(int value) {
         //bitCount compte le nombre de bits à l'état haut
         //Pour la représentation d'une puissance de 2 en binaire, seul 1 bit est à l'état haut
         return Long.bitCount(value) == 1;
     }
 
     /**
+     * Renvoie la taille du tableau
+     *
      * @return tailleP2
      */
     public int getTailleP2() {
--- src/edu/isen/fhgd/fft/FFTTest.java
+++ src/edu/isen/fhgd/fft/FFTTest.java
@@ -5,15 +5,15 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class FftTest {
+public class FFTTest {
     @Test
-    public void fftRapideComplexeExpo() throws Exception {
+    public void FFTCompexeExpo() throws Exception {
         Complexe[] signal = new Complexe[8];
         for (int i = 0; i < 8; i++) {
             signal[i] = new Complexe((float) (2 * Math.PI * i));
         }
-        Fft tranformee = new Fft(8, signal);
-        Complexe[] sortie = tranformee.fftRapideComplexe();
+        FFT tranformee = new FFT(8, signal);
+        Complexe[] sortie = tranformee.FFTComplexe();
 
         for (int i = 0; i < 8; i++) {
             System.out.println(sortie[i]);
@@ -21,14 +21,14 @@ public class FftTest {
     }
 
     @Test
-    public void fftRapideComplexeDirac() throws Exception {
+    public void FFTComplexeDirac() throws Exception {
         Complexe[] valeurDirac = new Complexe[8];
         valeurDirac[0]=new Complexe(1,0);
         for (int i = 1; i < 8; i++) {
             valeurDirac[i]=new Complexe(0,0);
         }
-        Fft dirac= new Fft(8, valeurDirac);
-        Complexe[] sortie = dirac.fftRapideComplexe();
+        FFT dirac= new FFT(8, valeurDirac);
+        Complexe[] sortie = dirac.FFTComplexe();
         for(int i=0;i<valeurDirac.length;i++)
         {
             System.out.println("i = " + i + " : " + sortie[i]);
@@ -36,13 +36,13 @@ public class FftTest {
     }
 
     @Test
-    public void fftRapideReel() {
+    public void FFTRelle() {
         float[] signal = new float[8];
         for (int i = 0; i < 8; i++) {
             System.out.println(signal[i]);
         }
-        Fft tranformee = new Fft(8, signal);
-        Complexe[] sortie = tranformee.fftRapideReel();
+        FFT tranformee = new FFT(8, signal);
+        Complexe[] sortie = tranformee.FFTRelle();
 
         for (int i = 0; i < 8; i++) {
             System.out.println(sortie[i]);
@@ -50,11 +50,11 @@ public class FftTest {
     }
 
     @Test
-    public void Fft() throws Exception
+    public void FFT() throws Exception
     {
         try {
             float test[] = {0,0};
-            Fft objet = new Fft(51, test);
+            FFT objet = new FFT(51, test);
             fail("Expected an IllegalArgumentException to be thrown");
         }catch(IllegalArgumentException aExp){
             assert(aExp.getMessage().contains("La taille n'est pas une puissance de 2"));
@@ -62,7 +62,7 @@ public class FftTest {
 
         try {
             float test[] = {0,0};
-            Fft objet = new Fft(64, test);
+            FFT objet = new FFT(64, test);
         }catch(IllegalArgumentException aExp){
             assert(aExp.getMessage().contains("La taille n'est pas une puissance de 2"));
             fail("No error expected");
